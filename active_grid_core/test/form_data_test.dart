731246dbd2797faad535e33a49d0cea5ee2fd734
Anton Borries <anton.borries@2denker.de>
2020-12-07 14:25:36 +0100

    Add Parsing Test for FormData

---
+++ active_grid_core/test/form_data_test.dart
@@ -0,0 +1,105 @@
+import 'package:active_grid_core/active_grid_core.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final title = 'title';
+  final response = {
+    'schema': {
+      'type': 'object',
+      'properties': {
+        'TextC': {
+          'type': 'string'
+        },
+        'NumberC': {
+          'type': 'integer'
+        },
+        'DateTimeC': {
+          'type': 'string',
+          'format': 'date-time'
+        },
+        'DateC': {
+          'type': 'string',
+          'format': 'date'
+        },
+        'CheckmarkC': {
+          'type': 'boolean'
+        }
+      },
+      'required': []
+    },
+    'actions': [
+      {
+        'uri': '/api/a/3ojhtqiltc0kiylfp8nddmxmk',
+        'method': 'POST'
+      }
+    ],
+    'components': [
+      {
+        'property': 'TextC',
+        'value': null,
+        'required': false,
+        'options': {
+          'multi': false,
+          'placeholder': '',
+          'description': '',
+          'label': null
+        },
+        'type': 'textfield'
+      },
+      {
+        'property': 'NumberC',
+        'value': null,
+        'required': false,
+        'options': {
+          'multi': false,
+          'placeholder': '',
+          'description': '',
+          'label': null
+        },
+        'type': 'textfield'
+      },
+      {
+        'property': 'DateTimeC',
+        'value': null,
+        'required': false,
+        'options': <String, dynamic>{},
+        'type': 'datePicker'
+      },
+      {
+        'property': 'DateC',
+        'value': null,
+        'required': false,
+        'options': <String, dynamic>{},
+        'type': 'datePicker'
+      },
+      {
+        'property': 'CheckmarkC',
+        'value': null,
+        'required': false,
+        'options': <String, dynamic>{},
+        'type': 'checkbox'
+      }
+    ],
+    'title': title
+  };
+  
+  group('Parsing', () {
+    test('Successful', () {
+      final formData = FormData.fromJson(response);
+      
+      expect(formData.title, title);
+      
+      expect(formData.actions.length, 1);
+      
+      expect(formData.components.length, 5);
+      
+      expect(formData.components.map((e) => e.runtimeType).toList(), [
+        FormComponentText,
+        FormComponentNumber,
+        FormComponentDateTime,
+        FormComponentDate,
+        FormComponentCheckBox,
+      ]);
+    });
+  });
+}
