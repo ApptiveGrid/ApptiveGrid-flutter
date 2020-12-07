51833148192eccddd78cd31acbfeeeffcb955aa0
Anton Borries <anton.borries@2denker.de>
2020-12-07 14:25:25 +0100

    Add Parsing Test for Form Action

---
+++ active_grid_core/test/form_action_test.dart
@@ -0,0 +1,21 @@
+import 'package:active_grid_core/active_grid_core.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Parsing',() {
+    test('Successful Parse', () {
+      final uri = '/api/a/3ojhtqiltc0kiylfp8nddmxmk';
+      final method = 'POST';
+
+      final json = {
+        'uri': uri,
+        'method': method,
+      };
+
+      final action = FormAction.fromJson(json);
+
+      expect(action.uri, uri);
+      expect(action.method, method);
+    });
+  });
+}
