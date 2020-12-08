4a2a966310ceb08633ee5dc6204b98c964990dfe
Gabriel Meira <gabriel.meira@2denker.de>
2020-12-08 15:15:44 +0100

    added test to not display title

--- active_grid_form/test/active_grid_form_test.dart
+++ active_grid_form/test/active_grid_form_test.dart
@@ -6,7 +6,6 @@ import 'package:active_grid_core/active_grid_model.dart' as model;
 import 'common.dart';
 
 void main() {
-
   group('Title', () {
     testWidgets('Title Displays', (tester) async {
       final client = MockActiveGridClient();
@@ -17,13 +16,32 @@ void main() {
         ),
       );
 
-      when(client.loadForm(formId: 'form')).thenAnswer((realInvocation) async => model.FormData('Form Title', [], [], {}));
+      when(client.loadForm(formId: 'form')).thenAnswer(
+          (realInvocation) async => model.FormData('Form Title', [], [], {}));
 
       await tester.pumpWidget(target);
       await tester.pumpAndSettle();
 
       expect(find.text('Form Title'), findsOneWidget);
     });
-  });
 
+    testWidgets('Title do not displays', (tester) async {
+      final client = MockActiveGridClient();
+      final target = TestApp(
+        client: client,
+        child: ActiveGridForm(
+          formId: 'form',
+          hideTitle: true,
+        ),
+      );
+
+      when(client.loadForm(formId: 'form')).thenAnswer(
+          (realInvocation) async => model.FormData('Form Title', [], [], {}));
+
+      await tester.pumpWidget(target);
+      await tester.pumpAndSettle();
+
+      expect(find.text('Form Title'), findsNothing);
+    });
+  });
 }
