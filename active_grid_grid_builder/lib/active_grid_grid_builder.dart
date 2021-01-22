7860f594ab3dd4fdf0b021e89a67e0c2545d9319
Anton Borries <anton.borries@2denker.de>
2021-01-22 10:17:47 +0100

    Add more documentation

--- active_grid_grid_builder/lib/active_grid_grid_builder.dart
+++ active_grid_grid_builder/lib/active_grid_grid_builder.dart
@@ -8,7 +8,7 @@ export 'package:active_grid_core/active_grid_core.dart';
 
 /// Builder for building Widgets that use [Grid] as the Data Source
 class ActiveGridGridBuilder extends StatefulWidget {
-  ///
+  /// Creates a Builder Widet
   const ActiveGridGridBuilder({
     Key key,
     @required this.user,
@@ -18,16 +18,26 @@ class ActiveGridGridBuilder extends StatefulWidget {
     @required this.builder,
   }) : super(key: key);
 
+  /// id of the user who owns the grid
   final String user;
+
+  /// id the space is in
   final String space;
+
+  /// id of the grid
   final String grid;
+
+  /// Initial [Grid] data that should be shown
   final Grid initialData;
+
+  /// Callback that is used to build the widget
   final Widget Function(BuildContext, AsyncSnapshot<Grid>) builder;
 
   @override
   ActiveGridGridBuilderState createState() => ActiveGridGridBuilderState();
 }
 
+/// State of a [ActiveGridGridBuilder] Widget
 class ActiveGridGridBuilderState extends State<ActiveGridGridBuilder> {
   AsyncSnapshot<Grid> _snapshot;
 
@@ -49,6 +59,9 @@ class ActiveGridGridBuilderState extends State<ActiveGridGridBuilder> {
     return widget.builder(context, _snapshot);
   }
 
+  /// Call this to reload the data.
+  ///
+  /// For example used when doing pull to refresh
   Future<void> reload({bool listen = false}) {
     return ActiveGrid.getClient(context, listen: listen)
         .loadGrid(user: widget.user, space: widget.space, grid: widget.grid)
