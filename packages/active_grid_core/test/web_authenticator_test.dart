c28c3a4ecf3f5a612df6f2d6f2a539c66f22f4e1
Anton Borries <anton.borries@2denker.de>
2021-04-27 14:35:04 +0200

    Try to write a test for web auth
    Did not find a way to test things depending on dart:html

---
+++ packages/active_grid_core/test/web_authenticator_test.dart
@@ -0,0 +1,22 @@
+import 'package:active_grid_core/network/web_authenticator.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'mocks.dart';
+
+@TestOn('browser')
+
+void main() {
+  testWidgets('Authenticate with Web', (tester) async {
+    // This does not really test the web implemetation
+    // Todo: Find a way to test thing depending on dart:html
+    final client = MockAuthClient();
+    final authenticator = Authenticator(
+      client,
+      scopes: ['openid'],
+      urlLauncher: (_) {},
+      redirectUri: Uri.parse('https://someurl.com')
+    );
+
+    await authenticator.authorize();
+  });
+}
