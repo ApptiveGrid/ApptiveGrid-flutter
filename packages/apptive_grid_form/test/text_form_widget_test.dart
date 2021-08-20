879876016e907bdbd05d89e5e64f168c4e42baf8
Anton Borries <anton.borries@2denker.de>
2021-08-20 08:59:42 +0200

    Fix Multiline TextField

---
+++ packages/apptive_grid_form/test/text_form_widget_test.dart
@@ -0,0 +1,30 @@
+import 'package:apptive_grid_form/apptive_grid_form.dart';
+import 'package:apptive_grid_form/widgets/apptive_grid_form_widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'common.dart';
+
+void main() {
+  testWidgets('Multiline TextFormWidget Displays', (tester) async {
+    final value = '''A
+multi-line
+string''';
+    final target = TextFormWidget(
+        component: StringFormComponent(
+            property: 'Text',
+            data: StringDataEntity(
+              value,
+            ),
+            options: TextComponentOptions(
+              multi: true,
+            ),
+            fieldId: 'Field'));
+
+    await tester.pumpWidget(TestApp(
+      child: target,
+    ));
+    await tester.pumpAndSettle();
+
+    expect(find.text(value), findsOneWidget);
+  });
+}
