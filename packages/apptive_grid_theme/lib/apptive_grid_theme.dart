26d5cfd23494c16faa0c1c6b7609586143ed4127
Anton Borries <anton.borries@2denker.de>
2021-09-24 12:19:06 +0200

    Fix Lint Issues

--- packages/apptive_grid_theme/lib/apptive_grid_theme.dart
+++ packages/apptive_grid_theme/lib/apptive_grid_theme.dart
@@ -18,106 +18,122 @@ class ApptiveGridTheme {
 
   /// Creates the theme
   ThemeData theme() {
-    final darkWindowBackground;
+    late final Color darkWindowBackground;
     if (Platform.isIOS) {
       darkWindowBackground = Colors.black;
     } else {
       darkWindowBackground = const Color(0xFF282625);
     }
-    final _windowBackground =
-        _withBrightness(light: Color(0xFFF7F7F7), dark: darkWindowBackground);
+    final _windowBackground = _withBrightness(
+      light: const Color(0xFFF7F7F7),
+      dark: darkWindowBackground,
+    );
     final baseTheme =
         _withBrightness(light: ThemeData.light(), dark: ThemeData.dark());
     final textTheme = _textTheme(baseTheme.textTheme);
 
     return baseTheme.copyWith(
-        primaryColor: ApptiveGridColors.apptiveGridBlue,
-        colorScheme: baseTheme.colorScheme.copyWith(
-            primary: ApptiveGridColors.apptiveGridBlue,
-            secondary: ApptiveGridColors.apptiveGridBlue),
-        toggleableActiveColor: ApptiveGridColors.apptiveGridBlue,
-        textSelectionTheme: baseTheme.textSelectionTheme.copyWith(
-            selectionHandleColor: ApptiveGridColors.apptiveGridBlue,
-            selectionColor: ApptiveGridColors.apptiveGridBlue.withOpacity(0.4)),
-        buttonTheme: baseTheme.buttonTheme.copyWith(
-          buttonColor: ApptiveGridColors.apptiveGridBlue,
-          textTheme: ButtonTextTheme.primary,
-        ),
-        hintColor: ApptiveGridColors.lightGrey.withOpacity(0.8),
-        inputDecorationTheme: baseTheme.inputDecorationTheme.copyWith(
-          filled: true,
-          border: OutlineInputBorder(
-              borderSide: const BorderSide(
-                color: ApptiveGridColors.apptiveGridBlue,
-              ),
-              borderRadius: BorderRadius.circular(8)),
-          focusedBorder: OutlineInputBorder(
-              borderSide: const BorderSide(
-                color: ApptiveGridColors.apptiveGridBlue,
-              ),
-              borderRadius: BorderRadius.circular(8)),
-          errorBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: baseTheme.errorColor,
-              ),
-              borderRadius: BorderRadius.circular(8)),
-          focusedErrorBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                color: baseTheme.errorColor,
-              ),
-              borderRadius: BorderRadius.circular(8)),
-        ),
-        dialogTheme: baseTheme.dialogTheme.copyWith(
-          titleTextStyle: textTheme.headline6,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-        ),
-        scaffoldBackgroundColor: _windowBackground,
-        textTheme: textTheme,
-        cardTheme: baseTheme.cardTheme.copyWith(
-          clipBehavior: Clip.hardEdge,
-          elevation: 0,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-        appBarTheme: baseTheme.appBarTheme.copyWith(
-          color: Colors.transparent,
-          elevation: 0,
-          centerTitle: false,
-          actionsIconTheme: const IconThemeData(
+      primaryColor: ApptiveGridColors.apptiveGridBlue,
+      colorScheme: baseTheme.colorScheme.copyWith(
+        primary: ApptiveGridColors.apptiveGridBlue,
+        secondary: ApptiveGridColors.apptiveGridBlue,
+      ),
+      toggleableActiveColor: ApptiveGridColors.apptiveGridBlue,
+      textSelectionTheme: baseTheme.textSelectionTheme.copyWith(
+        selectionHandleColor: ApptiveGridColors.apptiveGridBlue,
+        selectionColor: ApptiveGridColors.apptiveGridBlue.withOpacity(0.4),
+      ),
+      buttonTheme: baseTheme.buttonTheme.copyWith(
+        buttonColor: ApptiveGridColors.apptiveGridBlue,
+        textTheme: ButtonTextTheme.primary,
+      ),
+      hintColor: ApptiveGridColors.lightGrey.withOpacity(0.8),
+      inputDecorationTheme: baseTheme.inputDecorationTheme.copyWith(
+        filled: true,
+        border: OutlineInputBorder(
+          borderSide: const BorderSide(
             color: ApptiveGridColors.apptiveGridBlue,
           ),
-          iconTheme: IconThemeData(
-              color: _withBrightness(
-                  light: ApptiveGridColors.darkGrey, dark: Colors.white)),
-          brightness: brightness,
-          textTheme: textTheme,
-        ));
+          borderRadius: BorderRadius.circular(8),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(
+            color: ApptiveGridColors.apptiveGridBlue,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: baseTheme.errorColor,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: baseTheme.errorColor,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+      dialogTheme: baseTheme.dialogTheme.copyWith(
+        titleTextStyle: textTheme.headline6,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      ),
+      scaffoldBackgroundColor: _windowBackground,
+      textTheme: textTheme,
+      cardTheme: baseTheme.cardTheme.copyWith(
+        clipBehavior: Clip.hardEdge,
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      appBarTheme: baseTheme.appBarTheme.copyWith(
+        color: Colors.transparent,
+        elevation: 0,
+        centerTitle: false,
+        actionsIconTheme: const IconThemeData(
+          color: ApptiveGridColors.apptiveGridBlue,
+        ),
+        iconTheme: IconThemeData(
+          color: _withBrightness(
+            light: ApptiveGridColors.darkGrey,
+            dark: Colors.white,
+          ),
+        ),
+        brightness: brightness,
+        textTheme: textTheme,
+      ),
+    );
   }
 
   TextTheme _textTheme(TextTheme baseTheme) {
     final newTheme = baseTheme.copyWith(
-        bodyText1: const TextStyle(
-          fontSize: 14,
-        ),
-        headline3: const TextStyle(
-          fontSize: 44,
-          fontWeight: FontWeight.bold,
-        ),
-        headline5: const TextStyle(
-          fontSize: 28,
-          fontWeight: FontWeight.bold,
-        ),
-        headline6: const TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ));
+      bodyText1: const TextStyle(
+        fontSize: 14,
+      ),
+      headline3: const TextStyle(
+        fontSize: 44,
+        fontWeight: FontWeight.bold,
+      ),
+      headline5: const TextStyle(
+        fontSize: 28,
+        fontWeight: FontWeight.bold,
+      ),
+      headline6: const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+    );
     return newTheme.apply(
       bodyColor: _withBrightness(
-          light: ApptiveGridColors.lightGrey, dark: Colors.white54),
+        light: ApptiveGridColors.lightGrey,
+        dark: Colors.white54,
+      ),
       displayColor: _withBrightness(
-          light: ApptiveGridColors.darkGrey, dark: Colors.white),
+        light: ApptiveGridColors.darkGrey,
+        dark: Colors.white,
+      ),
       fontFamily: 'DMSans',
     );
   }
--- packages/apptive_grid_theme/test/apptive_grid_theme_test.dart
+++ packages/apptive_grid_theme/test/apptive_grid_theme_test.dart
@@ -4,12 +4,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:apptive_grid_theme/apptive_grid_theme.dart';
 
 class _TestApp extends StatelessWidget {
-  _TestApp(
-      {Key? key,
-      required AppBar? Function(ThemeData data) this.appBarBuilder,
-      bool this.isDark = false,
-      required Widget Function(ThemeData data) this.childBuilder})
-      : super(key: key);
+  _TestApp({
+    Key? key,
+    required this.appBarBuilder,
+    this.isDark = false,
+    required this.childBuilder,
+  }) : super(key: key);
 
   final Widget Function(ThemeData data) childBuilder;
   final bool isDark;
@@ -36,9 +36,12 @@ void main() {
   String _goldenFilePath(String name, {bool isDark = false}) =>
       'goldenFiles/' + (isDark ? 'dark' : 'light') + '/' + name + '.png';
 
-  void _test(Widget Function(ThemeData data) testableBuilder, String name,
-      WidgetTester widgetTester,
-      {AppBar? Function(ThemeData data)? appBarBuilder}) async {
+  void _test(
+    Widget Function(ThemeData data) testableBuilder,
+    String name,
+    WidgetTester widgetTester, {
+    AppBar? Function(ThemeData data)? appBarBuilder,
+  }) async {
     appBarBuilder ??= (data) => null;
     final testApp = _TestApp(
       appBarBuilder: appBarBuilder,
@@ -47,7 +50,9 @@ void main() {
 
     await widgetTester.pumpWidget(testApp);
     await expectLater(
-        find.byType(_TestApp), matchesGoldenFile(_goldenFilePath(name)));
+      find.byType(_TestApp),
+      matchesGoldenFile(_goldenFilePath(name)),
+    );
 
     final testAppDark = _TestApp(
       isDark: true,
@@ -56,201 +61,213 @@ void main() {
     );
 
     await widgetTester.pumpWidget(testAppDark);
-    await expectLater(find.byType(_TestApp),
-        matchesGoldenFile(_goldenFilePath(name, isDark: true)));
+    await expectLater(
+      find.byType(_TestApp),
+      matchesGoldenFile(_goldenFilePath(name, isDark: true)),
+    );
   }
 
   testWidgets('TextField theme', (widgetTester) async {
     _test(
-        (data) => TextField(
-              style: data.textTheme.bodyText1,
-              controller: TextEditingController(
-                text: 'Test',
-              ),
-            ),
-        'TextField',
-        widgetTester);
+      (data) => TextField(
+        style: data.textTheme.bodyText1,
+        controller: TextEditingController(
+          text: 'Test',
+        ),
+      ),
+      'TextField',
+      widgetTester,
+    );
   });
 
   testWidgets('Elevated button theme', (widgetTester) async {
     _test(
-        (data) => ElevatedButton(
-              onPressed: null,
-              style: data.elevatedButtonTheme.style,
-              child: Text(
-                'Button',
-                style: data.textTheme.button,
-              ),
-            ),
-        'ElevatedButton',
-        widgetTester);
+      (data) => ElevatedButton(
+        onPressed: null,
+        style: data.elevatedButtonTheme.style,
+        child: Text(
+          'Button',
+          style: data.textTheme.button,
+        ),
+      ),
+      'ElevatedButton',
+      widgetTester,
+    );
   });
 
   testWidgets('Text button theme', (widgetTester) async {
     _test(
-        (data) => TextButton(
-              onPressed: null,
-              style: data.elevatedButtonTheme.style,
-              child: Text(
-                'Button',
-                style: data.textTheme.button,
-              ),
-            ),
-        'TextButton',
-        widgetTester);
+      (data) => TextButton(
+        onPressed: null,
+        style: data.elevatedButtonTheme.style,
+        child: Text(
+          'Button',
+          style: data.textTheme.button,
+        ),
+      ),
+      'TextButton',
+      widgetTester,
+    );
   });
 
   testWidgets('Text form field theme', (widgetTester) async {
     _test(
-        (data) => TextFormField(
-              initialValue: 'Test',
-              style: data.textTheme.bodyText1,
-            ),
-        'TextFormField',
-        widgetTester);
+      (data) => TextFormField(
+        initialValue: 'Test',
+        style: data.textTheme.bodyText1,
+      ),
+      'TextFormField',
+      widgetTester,
+    );
   });
 
   testWidgets('Popup menu button theme', (widgetTester) async {
     _test(
-        (data) => PopupMenuButton(
-              itemBuilder: (context) => [],
-            ),
-        'PopupMenuButton',
-        widgetTester);
+      (data) => PopupMenuButton(
+        itemBuilder: (context) => [],
+      ),
+      'PopupMenuButton',
+      widgetTester,
+    );
   });
 
   testWidgets('Icon button theme', (widgetTester) async {
     _test(
-        (data) => IconButton(
-              icon: Text(
-                'Test',
-                style: data.textTheme.button,
-              ),
-              onPressed: null,
-            ),
-        'IconButton',
-        widgetTester);
+      (data) => IconButton(
+        icon: Text(
+          'Test',
+          style: data.textTheme.button,
+        ),
+        onPressed: null,
+      ),
+      'IconButton',
+      widgetTester,
+    );
   });
 
   testWidgets('Card theme', (widgetTester) async {
     _test(
-        (data) => Card(
-              child: Text(
-                'Test',
-                style: data.textTheme.bodyText1,
-              ),
-            ),
-        'Card',
-        widgetTester);
+      (data) => Card(
+        child: Text(
+          'Test',
+          style: data.textTheme.bodyText1,
+        ),
+      ),
+      'Card',
+      widgetTester,
+    );
   });
 
   testWidgets('Alert dialog theme', (widgetTester) async {
     _test(
-        (data) => AlertDialog(
-              title: Text(
-                'Test',
-                style: data.textTheme.bodyText1,
-              ),
-            ),
-        'AlertDialog',
-        widgetTester);
+      (data) => AlertDialog(
+        title: Text(
+          'Test',
+          style: data.textTheme.bodyText1,
+        ),
+      ),
+      'AlertDialog',
+      widgetTester,
+    );
   });
 
   testWidgets('App bar theme', (widgetTester) async {
     _test(
-        (data) => Text(
-              'Body',
-              style: data.textTheme.bodyText1,
-            ),
-        'AppBar',
-        widgetTester,
-        appBarBuilder: (data) => AppBar(
-              title: Text(
-                'Test',
-                style: data.textTheme.headline5,
-              ),
-            ));
+      (data) => Text(
+        'Body',
+        style: data.textTheme.bodyText1,
+      ),
+      'AppBar',
+      widgetTester,
+      appBarBuilder: (data) => AppBar(
+        title: Text(
+          'Test',
+          style: data.textTheme.headline5,
+        ),
+      ),
+    );
   });
 
   testWidgets('Text theme', (widgetTester) async {
     _test(
-        (data) => Column(
-              children: [
-                Expanded(
-                  child: Text(
-                    'test',
-                    style: data.textTheme.bodyText1,
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    'test',
-                    style: data.textTheme.bodyText2,
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    'test',
-                    style: data.textTheme.caption,
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    'test',
-                    style: data.textTheme.headline1,
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    'test',
-                    style: data.textTheme.headline2,
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    'test',
-                    style: data.textTheme.headline3,
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    'test',
-                    style: data.textTheme.headline4,
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    'test',
-                    style: data.textTheme.headline5,
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    'test',
-                    style: data.textTheme.headline6,
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    'test',
-                    style: data.textTheme.overline,
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    'test',
-                    style: data.textTheme.subtitle1,
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    'test',
-                    style: data.textTheme.subtitle2,
-                  ),
-                ),
-              ],
+      (data) => Column(
+        children: [
+          Expanded(
+            child: Text(
+              'test',
+              style: data.textTheme.bodyText1,
             ),
-        'Text',
-        widgetTester);
+          ),
+          Expanded(
+            child: Text(
+              'test',
+              style: data.textTheme.bodyText2,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'test',
+              style: data.textTheme.caption,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'test',
+              style: data.textTheme.headline1,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'test',
+              style: data.textTheme.headline2,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'test',
+              style: data.textTheme.headline3,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'test',
+              style: data.textTheme.headline4,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'test',
+              style: data.textTheme.headline5,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'test',
+              style: data.textTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'test',
+              style: data.textTheme.overline,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'test',
+              style: data.textTheme.subtitle1,
+            ),
+          ),
+          Expanded(
+            child: Text(
+              'test',
+              style: data.textTheme.subtitle2,
+            ),
+          ),
+        ],
+      ),
+      'Text',
+      widgetTester,
+    );
   });
 }
