431ea8417f06d8fd3f9dde4bd81573093913eee8
Anton Borries <anton.borries@2denker.de>
2021-09-24 10:36:38 +0200

    Prepare Testing
    Adding proper test might take a while as it is not 100% straight forward to mock the html dependency. One option would be to run the tests via integration_test however I'm still not 100% sure on how to mock the necessary data

--- packages/apptive_grid_web_apptive/test/apptive_grid_extension_test.dart
+++ packages/apptive_grid_web_apptive/test/apptive_grid_extension_test.dart
@@ -1,5 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:apptive_grid_web_apptive/apptive_grid_web_apptive.dart';
 
-void main() {}
+void main() {
+
+  testWidgets('Some Test', (tester) async {
+    final webApptive = ApptiveGridWebApptive(builder: (context, event) => SizedBox());
+    await tester.pumpWidget(webApptive);
+  });
+}
