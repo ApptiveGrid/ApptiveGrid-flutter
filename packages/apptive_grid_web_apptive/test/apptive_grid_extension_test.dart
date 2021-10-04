525a83383624af04dfad1bade15bde69a6d5b9fe
Anton Borries <anton.borries@2denker.de>
2021-10-04 16:28:24 +0200

    Fix Test runs

--- packages/apptive_grid_web_apptive/test/apptive_grid_extension_test.dart
+++ packages/apptive_grid_web_apptive/test/apptive_grid_extension_test.dart
@@ -1,3 +1,5 @@
+@TestOn('browser')
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
---
+++ packages/apptive_grid_web_apptive/test/more_tests_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('You should also run melos run test:web locally', () {
+    debugPrint('Please also execute: `melos run test:web` locally');
+  });
+}
