a0daad3c27e2ce33ac05b553cd568e97a2adb554
Sören Schmaljohann <soeren.schmaljohann@2denker.de>
2022-10-07 15:45:48 +0200

    extract issuer creation

--- packages/apptive_grid_core/lib/src/network/authentication/web_auth_enabler/configure_web.dart
+++ packages/apptive_grid_core/lib/src/network/authentication/web_auth_enabler/configure_web.dart
@@ -3,6 +3,7 @@
 import 'dart:html';
 
 import 'package:apptive_grid_core/apptive_grid_core.dart';
+import 'package:apptive_grid_core/src/network/authentication/apptive_grid_authenticator.dart';
 import 'package:apptive_grid_core/src/network/authentication/web_authenticator.dart';
 import 'package:openid_client/openid_client.dart' as openid;
 
@@ -11,10 +12,7 @@ import 'package:openid_client/openid_client.dart' as openid;
 Future<void> enableWebAuth(ApptiveGridOptions options) async {
   final url = window.location.href;
   if (url.contains('state=')) {
-    final issuerUri = Uri.parse(
-      '${options.environment.url}/auth/${options.authenticationOptions.authGroup}/authorize',
-    );
-    final issuer = await openid.Issuer.discover(issuerUri);
+    final issuer = ApptiveGridAuthenticator.getIssuerWithUri(options);
     final client = openid.Client(issuer, 'web');
     final webAuthenticator = Authenticator(client, redirectUri: Uri.parse(url));
     await webAuthenticator.authorize();
