82712cb886d35bb2b3f1c152f13952536955d76a
Sören Schmaljohann <soeren.schmaljohann@2denker.de>
2022-10-25 13:24:12 +0200

    cleanup

--- packages/apptive_grid_form/lib/src/widgets/form_widget/email_form_widget.dart
+++ packages/apptive_grid_form/lib/src/widgets/form_widget/email_form_widget.dart
@@ -2,7 +2,6 @@ import 'package:apptive_grid_form/apptive_grid_form.dart';
 import 'package:apptive_grid_form/src/translation/apptive_grid_localization.dart';
 import 'package:apptive_grid_form/src/widgets/form_widget/form_widget_helpers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 /// FormComponent Widget to display a [FormComponent<EmailDataEntity>]
 class EmailFormWidget extends StatefulWidget {
