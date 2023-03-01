f1686ce32a1001186b7c0695224ab863c2842595
Anton Borries <anton.borries@2denker.de>
2023-03-01 15:15:32 +0100

    Add variable to ApptiveGridCache to determine if pending actions should be auto send

--- packages/apptive_grid_core/lib/src/cache/apptive_grid_cache.dart
+++ packages/apptive_grid_core/lib/src/cache/apptive_grid_cache.dart
@@ -4,6 +4,10 @@ import 'package:apptive_grid_core/apptive_grid_core.dart';
 
 /// Cache for saving Actions to a cache to enable sending them later
 abstract class ApptiveGridCache {
+  /// Determines if the pending Actions should be automatically be send when the ApptiveGrid Widget is created.
+  /// Defaults to `true`
+  bool get shouldAutomaticallySendPendingActions => true;
+
   /// Add [actionItem] to the cache
   FutureOr<void> addPendingActionItem(ActionItem actionItem);
 
