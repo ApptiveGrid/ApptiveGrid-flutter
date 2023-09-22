2ecd62314258cc14912cad466dddeaed838254ed
Sören Schmaljohann <soeren.schmaljohann@2denker.de>
2023-09-22 10:19:01 +0200

    add new properties

--- packages/apptive_grid_core/lib/src/model/form/form_field_properties.dart
+++ packages/apptive_grid_core/lib/src/model/form/form_field_properties.dart
@@ -9,6 +9,9 @@ class FormFieldProperties {
         fieldId: fieldId,
         pageId: json['pageId'],
         fieldIndex: json['fieldIndex'],
+        defaultValue: json['defaultValue'],
+        disabled: json['disabled'],
+        hidden: json['hidden'],
       );
 
   /// Creates a FormFieldProperties Object
@@ -16,6 +19,9 @@ class FormFieldProperties {
     required this.fieldId,
     this.pageId,
     this.fieldIndex,
+    this.defaultValue,
+    this.disabled = false,
+    this.hidden = false,
   });
 
   /// Id of the field the properties belong to
@@ -26,4 +32,13 @@ class FormFieldProperties {
 
   /// Position on the page
   final int? fieldIndex;
+
+  /// Default value of the form field
+  final dynamic defaultValue;
+
+  /// Flag whether the form field is hidden. This usually means it also has a [defaultValue] set
+  final bool hidden;
+
+  /// Flag whether the form field is read-only
+  final bool disabled;
 }
